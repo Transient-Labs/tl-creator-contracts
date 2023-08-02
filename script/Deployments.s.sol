8515e2d98d608a8db436b72f9b2725505504ff3a
Marco Peyfuss <mpeyfuss@gmail.com>
2023-08-02 06:49:52 -0600

    updated makefile for testing and deployment. wrote a deployment script. updated foundry.toml. deployed version 2.5.0 contracts.

---
+++ script/Deployments.s.sol
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.19;
+
+import "forge-std/Script.sol";
+import {ERC721TL} from "tl-creator-contracts/core/ERC721TL.sol";
+import {ERC1155TL} from "tl-creator-contracts/core/ERC1155TL.sol";
+import {Shatter} from "tl-creator-contracts/shatter/Shatter.sol";
+
+contract DeployERC721TL is Script {
+
+    function run() public {
+        vm.broadcast();
+        new ERC721TL(true);
+    }
+}
+
+contract DeployERC1155TL is Script {
+
+    function run() public {
+        vm.broadcast();
+        new ERC1155TL(true);
+    }
+}
+
+contract DeployShatter is Script {
+
+    function run() public {
+        vm.broadcast();
+        new Shatter(true);
+    }
+}
